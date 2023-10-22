0e15f841ca4b7228c44145ad15d93da66e79c377
I-Wont-Use-This-Account <71619195+I-Wont-Use-This-Account@users.noreply.github.com>
2023-10-21 22:59:41 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzX8EXM-UOOiPTM4vdeDBHnXrI9WMxtY0",
+  authDomain: "project71-704f1.firebaseapp.com",
+  projectId: "project71-704f1",
+  storageBucket: "project71-704f1.appspot.com",
+  messagingSenderId: "1042955322044",
+  appId: "1:1042955322044:web:395cf4eb4d0b002ebc06b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
